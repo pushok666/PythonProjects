d44b3be2dd0ce43d9112eae8df139cb5a8362c62
bloknet <barinov_tosha@inbox.ru>
2021-10-30 12:42:25 +0300

    fix app

--- firstPyQt/testQt.py
+++ firstPyQt/testQt.py
@@ -4,7 +4,6 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLay
 
 app = QApplication([])#создание объекта приложения
 main_win = QWidget()#создание окна
-main_win.move(500,500)#установка размера окна
 main_win.setWindowTitle("My Firs PyQt App!") #заголовок главного окна
 hello_text = QLabel("Hello Windows")#запись в окне
 
@@ -12,5 +11,6 @@ line_map = QVBoxLayout()
 line_map.addWidget(hello_text, alignment = Qt.AlignCenter)#отрисовка записи по цетру
 main_win.setLayout(line_map)#добавление записи в окно   
 
+main_win.resize(800,600)
 main_win.show()
 app.exec_()
